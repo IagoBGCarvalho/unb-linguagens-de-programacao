108b3303fb650dfeff56be3b0a27192cf7addb6c
Iago Carvalho <iagobgc@gmail.com>
2026-08-19 20:49:58 -0300

    feat: adições da aula 03

--- 1-haskell_basico/1-programacao_baseada_em_definicoes.hs
+++ 1-haskell_basico/1-programacao_baseada_em_definicoes.hs
@@ -4,7 +4,8 @@
 -- 3 - Digitar ":load nome_arquivo.sh" para carregar o código na memória
 -- 4 - Caso o console retorne "Ok, one module loaded" a partir desse momento todas as funções do arquivo estarão na memória e podem ser invocadas pelos seus nomes
 -- 5 - Basta digitar o nome da função seguida de seus parâmetros para retornar a sua saída (ex: quadrado 4)
--- 6 - Para sair do modo interativo e voltar para o terminal, basta digitar ":quit"
+-- 6 - Para carregar alterações feitas após o interpretador ser aberto, basta digitar ":r"
+-- 7 - Para sair do modo interativo e voltar para o terminal, basta digitar ":quit"
 
 -- Definindo constantes
 resposta :: Int
@@ -31,7 +32,48 @@ maxi n m | n >= m    = n
 addD :: Int -> Int -> Int
 addD a b = 2 * (a+b)
 
+-- Como o Haskell resolve os problemas (lazy valuation):
 -- addD 2 (addD 3 4)
 -- = 2 * (2 + (addD 3 4))
 -- = 2 * (2 + 2 * (3 + 4))
 -- = 32
+
+-- Tipos básicos
+
+-- Inteiros:
+-- 1, 2, 3, ... :: Int
+-- +, *, -, div, mod :: Int -> Int -> Int
+-- >, >=, ==, /=, <=, < :: Int -> Int -> Bool
+-- Int: Tamanho fixo por implementação (8/16 bytes)
+-- Integer: Tamanho arbitrário
+
+-- Booleanos:
+-- True, False :: Bool -> Bool
+-- &&, || :: Bool -> Bool -> Bool
+-- not :: Bool -> Bool
+
+-- Recursão: 
+-- Definir caso base
+-- Definir o valor para fun n usando o valor de fun n-1 (passo recursivo)
+
+-- Casamento de padrões:
+-- Permite usar padrões no lugar de variáveis na definição de funções, permitindo implementar comportamentos diferentes de função para padrões diferentes.
+-- A avaliação de padrões sempre é feita de cima para baixo na execução do código, assim como a guarda
+-- Exemplo:
+maxSales :: Int -> Int
+maxSales 0 = 0 -- O padrão é 0, ou sejam quando o argumento for 0, o comportamento será retornar 0
+maxSales n = maxi n 5 -- "n" é o padrão que será ativado quando qualquer argumento inteiro for utilizado, onde o compotamento da função será outro (verificar se o inteiro é maior do 5 e retornar o maior valor)
+
+-- Outros exemplo:
+myOr :: Bool -> Bool -> Bool
+myOr True  x = True
+myOr False x = x
+
+myAnd :: Bool -> Bool -> Bool
+myAnd False x = False
+myAnd True  x = x
+
+-- Exercícios:
+-- makeSpaces :: Int -> String
+-- makeSpaces n | (n == 0) = ""
+--              | (n > 0)  = chr n 
--- 1-haskell_basico/exercicios/fatorial.hs
+++ 1-haskell_basico/exercicios/fatorial.hs
@@ -1,4 +1,7 @@
 fatorial :: Int -> Int
 fatorial n | (n == 0) = 1
-           | (n == 1) = 1
            | otherwise = n * fatorial(n - 1)
+
+fatorial' :: Int -> Int
+fatorial' n | (n == 0) = 1
+            | (n > 0)  = n * fatorial'(n - 1) -- apesar de dar erro para números negativos (assim como a função anterior) ainda é um erro menos pior, pois não consome toda a memória e dá um stack overflow como a anterior
